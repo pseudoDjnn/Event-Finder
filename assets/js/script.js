f5b7f8bbf450459863e95240db4676d3b01eb15f
p5eudoDjnn <alexander.havers@gmail.com>
2022-08-27 14:07:15 -0400

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,14 @@ const CLIENT_ID = `Mjg2MTA4MDN8MTY2MTIxMjIzMS41NDk3NjAz`;
 
 btnForCallApi.addEventListener(`click`, callAPI);
 
-function callAPI () {
-    fetch(`https://api.seatgeek.com/2/events?client_id=${CLIENT_ID}`).then( (response) => {
-
-        return response.json()
-
-    }).then( (data) => {
-
-        console.log(data);
-
+function callAPI() {
+  fetch(
+    `https://api.seatgeek.com/2/events?geoip=true&client_id=${CLIENT_ID}&per_page=25&page=3&sort=score.desc`
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
     });
-};
+}
